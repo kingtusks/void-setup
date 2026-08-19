2c7f831f3d943e6dfc2ffbdd8bde6ea4ae34a2ea
Tirth Void <tirth11804@gmail.com>
2026-08-19 18:21:01 -0400

    Made a script to setup bluetooth

--- scripts/bluetooth.sh
+++ scripts/bluetooth.sh
@@ -1,1 +1,14 @@
 #/bin/sh
+
+rfkill unblock bluetooth
+
+sudo xbps-install -S bluez
+sudo ln -s /etc/sv/bluetoothd /var/service/
+sudo ln -s /etc/sv/dbus /var/service/
+
+sudo usermod -aG bluetooth $USER
+
+sudo sv restart dbus
+
+# (IMPORTANT) if restarting dbus kills your wifi run the following command alone
+# sudo sv restart NetworkManager
