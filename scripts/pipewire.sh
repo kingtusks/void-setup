d427683c5bff63c45b3da6e67cce7ec259f8995e
Tirth Void <tirth11804@gmail.com>
2026-08-19 18:37:34 -0400

    added a script to setup pipewire (for audio)

---
+++ scripts/pipewire.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+sudo xbps-install -S pipewire
+
+#connecting to wireplumber
+sudo mkdir -p /etc/pipewire/pipewire.conf.d
+sudo ln -s /usr/share/examples/wireplumber/10-wireplumber.conf /etc/pipewire/pipewire.conf.d/
+sudo ln -s /usr/share/examples/pipewire/20-pipewire-pulse.conf /etc/pipewire/pipewire.conf.d/
+
+#enabling as a service
+sudo mkdir -p ~/.config/service
+sudo ln -s /etc/sv/pipewire ~/.config/service/
+sudo ln -s /etc/sv/wireplumber ~/.config/service/
+sudo ln -s /etc/sv/pipewire-pulse ~/.config/service/
